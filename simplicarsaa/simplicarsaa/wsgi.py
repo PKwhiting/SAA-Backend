15112698c711832402233ade7dbd878ac6cc31ce
Patten Whiting <pattenwhiting@Pattens-MacBook-Pro.local>
2023-11-13 00:14:23 -0700

    wsgi update

--- simplicarsaa/simplicarsaa/wsgi.py
+++ simplicarsaa/simplicarsaa/wsgi.py
@@ -12,5 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'simplicarsaa.settings')
+import sys
+sys.path.append('/opt/render/project/src/simplicarsaa/')
 
 application = get_wsgi_application()
