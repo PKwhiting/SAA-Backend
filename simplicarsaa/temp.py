d3b3d78b6939d77328a34c1b4dff733b92589b88
Patten Whiting <pattenwhiting@Pattens-MacBook-Pro.local>
2023-12-02 19:56:30 -0700

    car script

--- simplicarsaa/temp.py
+++ simplicarsaa/temp.py
@@ -13,10 +13,8 @@ from django.conf import settings
 def get_state(state_abbreviation):
     try:
         state = State.objects.get(state_abbr=state_abbreviation)
-        print(state)
         return state
     except State.DoesNotExist:
-        print("State does not exist")
         return None
 
 # Load the JSON data from file
@@ -26,49 +24,63 @@ with open('copart.json') as f:
 creator = User.objects.get(username='pattenwhiting')
 
 for car in car_data:
-    if car['vehicleCategory'] == 'Automobile' and car['saleDateTimeLeft']['isTimeLeft']:
-        print(car)
-        new_car = Car(
-            auction=AUCTION_CHOICES[1][0],
-            creator=creator,  # Replace with the creator of the car if applicable
-            year=car['year'],
-            make=car['make'],
-            model=car['model'],
-            VIN=car['vin'],
-            title_classification=car['title']['name'],
-            color=car['color'],
-            engine=car['engineSize'],
-            engine_displacement=car['engineSize'],
-            cylinders=car['cylinders'],
-            transmission=car['transmission'],
-            drive_type=car['drive'],
-            vehicle_type=car['bodyStyle'],
-            fuel_type=car['fuel'],
-            keys=1,
-            mileage=car['odometer'],
-            starting_bid=0,
-            current_bid=car['currentBid'],
-            reserve_price=0,
-            description=car['description'],
-            # active=car['saleDateTimeLeft']['isTimeLeft'],
-            active=True,
-            condition=f'{car["primaryDamage"]} {car["secondaryDamage"]}',
-            state=get_state(car['location']['stateCode']),
-            sale_date=car['saleDate'],
-            vehicle_auction_link = f'https://www.copart.com/lot/{car["id"]}',
-            image_1_url = car['images'][0]['full'] if len(car['images']) > 0 else None,
-            image_2_url = car['images'][1]['full'] if len(car['images']) > 1 else None,
-            image_3_url = car['images'][2]['full'] if len(car['images']) > 2 else None,
-            image_4_url = car['images'][3]['full'] if len(car['images']) > 3 else None,
-            image_5_url = car['images'][4]['full'] if len(car['images']) > 4 else None,
-            image_6_url = car['images'][5]['full'] if len(car['images']) > 5 else None,
-            image_7_url = car['images'][6]['full'] if len(car['images']) > 6 else None,
-            image_8_url = car['images'][7]['full'] if len(car['images']) > 7 else None,
-            image_9_url = car['images'][8]['full'] if len(car['images']) > 8 else None,
-            image_10_url = car['images'][9]['full'] if len(car['images']) > 9 else None,
-            vehicle_starts=False,
-            vehicle_drives=False
-        )
-        # Save the new Car object to the database
-        new_car.save()
+    try:
+        if car['vehicleCategory'] == 'Automobile' and car['saleDateTimeLeft']['isTimeLeft']:
+            location = car.get('saleLocation', {})
+            state_code = location.get('stateCode', '')
+            state = get_state(state_code) if state_code else None
+            buy_it_now_price=car.get('buyItNow', '')
+            if int(buy_it_now_price) > 0:
+                buy_it_now = True
+            else:
+                buy_it_now = False
 
+
+            new_car = Car(
+                auction=AUCTION_CHOICES[1][0],
+                creator=creator,
+                year=car.get('year', ''),
+                make=car.get('make', ''),
+                model=car.get('model', ''),
+                VIN=car.get('vin', ''),
+                title_classification="SALVAGE",
+                title_note=car.get('title', {}).get('name', ''),
+                color=car.get('color', ''),
+                engine=car.get('engineSize', ''),
+                engine_displacement=car.get('engineSize', ''),
+                cylinders=car.get('cylinders', ''),
+                transmission=car.get('transmission', ''),
+                drive_type=car.get('drive', ''),
+                vehicle_type=car.get('bodyStyle', ''),
+                fuel_type=car.get('fuel', ''),
+                buy_it_now_price=buy_it_now_price,
+                buy_it_now = buy_it_now,
+                keys=1,
+                mileage=car.get('odometer', ''),
+                odometer_brand='ACTUAL',
+                starting_bid=0,
+                current_bid=car.get('currentBid', ''),
+                reserve_price=0,
+                description=car.get('description', ''),
+                condition=f'{car.get("primaryDamage", "")} {car.get("secondaryDamage", "")}',
+                state=state,
+                active=False,
+                sale_date=car.get('saleDate', ''),
+                vehicle_auction_link=f'https://www.copart.com/lot/{car["id"]}',
+                image_1_url=car.get('images', [{}])[0].get('full', None),
+                image_2_url=car.get('images', [{}])[1].get('full', None),
+                image_3_url=car.get('images', [{}])[2].get('full', None),
+                image_4_url=car.get('images', [{}])[3].get('full', None),
+                image_5_url=car.get('images', [{}])[4].get('full', None),
+                image_6_url=car.get('images', [{}])[5].get('full', None),
+                image_7_url=car.get('images', [{}])[6].get('full', None),
+                image_8_url=car.get('images', [{}])[7].get('full', None),
+                image_9_url=car.get('images', [{}])[8].get('full', None),
+                image_10_url=car.get('images', [{}])[9].get('full', None),
+                vehicle_starts=False,
+                vehicle_drives=False
+            )
+            new_car.save()
+    except (TypeError, KeyError):
+        print("ERROR")
+        # print(TypeError, KeyError)
